8ea0567674ecf3d61790994cc28bb0847470b038
bitgoalsdeveloper <85306447+bitgoalsdeveloper@users.noreply.github.com>
2021-06-10 10:29:06 -0600

    Update PLT-Token-ERC20.sol

--- contracts/PLT-Token-ERC20.sol
+++ contracts/PLT-Token-ERC20.sol
@@ -4,9 +4,8 @@ pragma solidity =0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Poollotto is ERC20, ERC20Burnable, ERC20Permit, Ownable {
+contract Poollotto is ERC20, ERC20Burnable, ERC20Permit {
     constructor() ERC20("Poollotto", "PLT") ERC20Permit("Poollotto") {
         _mint(msg.sender, 19272000 * 10 ** decimals());
     }
