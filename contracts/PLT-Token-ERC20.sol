46b92d456d884ae8c217c8b7706de8c3ec94f779
bitgoalsdeveloper <85306447+bitgoalsdeveloper@users.noreply.github.com>
2021-06-11 07:27:00 -0600

    Update PLT-Token-ERC20.sol

--- contracts/PLT-Token-ERC20.sol
+++ contracts/PLT-Token-ERC20.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract Poollotto is ERC20, ERC20Burnable, ERC20Permit {
-    constructor() ERC20("Poollotto", "PLT") ERC20Permit("Poollotto") {
+    constructor() ERC20("Poollotto.finance", "PLT") ERC20Permit("Poollotto.finance") {
         _mint(msg.sender, 19272000 * 10 ** decimals());
     }
 }
